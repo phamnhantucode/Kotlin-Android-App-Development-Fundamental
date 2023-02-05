6f21510366e50399d80577a892a0ade9723d8b86
phamnhantucode <bao.trancong.2003@gmail.com>
2023-02-05 14:36:53 +0700

    SimpleAdapterExample

---
+++ Learning/Example/app/src/main/java/com/learning/example/Adapter/SimpleAdapterExample.kt
@@ -0,0 +1,11 @@
+package com.learning.example.Adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SimpleAdapterExample : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_adapter_example)
+    }
+}
